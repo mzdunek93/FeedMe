0c23a0873384e300dc48184ffc97a229d3f4b446
OldBen <krzysztof.buniewicz@gmail.com>
2016-06-12 18:50:40 +0200

    added unit tests for restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -10,4 +10,28 @@ RSpec.describe Restaurant, type: :model do
       address: "Testing 2")
     expect(restaurant).to be_invalid
   end
+  it "requires restaurant description not to be empty" do
+    restaurant = Restaurant.new(
+      name: "Testautant",
+      description: "",
+      phone: "123456789", 
+      address: "Testing 2")
+    expect(restaurant).to be_invalid
+  end
+  it "requires restaurant phone no. not to be empty" do
+    restaurant = Restaurant.new(
+      name: "Testaurant",
+      description: "This restaurant is supposed to create error",
+      phone: "", 
+      address: "Testing 2")
+    expect(restaurant).to be_invalid
+  end
+  it "requires restaurant address not to be empty" do
+    restaurant = Restaurant.new(
+      name: "Testaurant",
+      description: "This restaurant is supposed to create error",
+      phone: "123456789", 
+      address: "")
+    expect(restaurant).to be_invalid
+  end
 end
