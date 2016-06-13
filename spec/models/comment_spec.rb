61ff3c2770bcba345ac991c39afc7fb944b822ee
OldBen <krzysztof.buniewicz@gmail.com>
2016-06-13 17:51:46 +0200

    added unit tests for comment module

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @testRes = Restaurant.create!(
+      name: "TempRes", 
+      description:"Restaurant for testing", 
+      phone:"12 345 67 89",
+      address:"Test 1, Beta City")
+    @testUser = User.create!(
+      email: "user@test.com", 
+      name: "test", 
+      password: "password", 
+      password_confirmation: "password"  
+    )
+  end
+  it "allows user to post comment about a restaurant" do
+    com = Comment.new(
+    text: "Tasty restaurant",
+    user: @testUser,
+    restaurant: @testRes)
+    expect(com.text).to match "Tasty restaurant"
+  end
+  it "requires comment to be assigned to user" do
+    com = Comment.new(
+    text: "Tasty restaurant",
+    restaurant: @testRes)
+    expect(com).to be_invalid
+  end
+  it "requires comment to be assigned to restaurant" do
+    com = Comment.new(
+    text: "Tasty restaurant",
+    user: @testUser)
+    expect(com).to be_invalid    
+  end
+  it "requires comment to have text" do
+    com = Comment.new(
+    text: "",
+    user: @testUser,
+    restaurant: @testRes)
+
+  end
 end
